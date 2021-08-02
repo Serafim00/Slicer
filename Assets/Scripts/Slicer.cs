19f5057edb72dc20916d076b3c482803050b9946
Serafim00 <ser.kokhov@gmail.com>
2021-08-02 11:01:31 +0300

    3 game
    3 game

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using BzKovSoft.ObjectSlicer;
+using BzKovSoft.ObjectSlicerSamples;
+
+public class Slicer : MonoBehaviour
+{
+    [SerializeField] private GameObject _blade;
+    [SerializeField] private float _duration;
+    [SerializeField] private float _offsetY;
+
+    private BzKnife _knife;
+
+    private void Start()
+    {
+        _knife = _blade.GetComponentInChildren<BzKnife>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _knife.BeginNewSlice();
+
+            _blade.transform.DOMoveY(_blade.transform.position.y - _offsetY, _duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+}
